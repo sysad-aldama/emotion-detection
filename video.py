24009e26ae32e1f484e43eb28d5facdd045f5d86
JP Aldama <sysad.aldama@gmail.com>
2020-03-13 22:11:35 -0400

    clean-up the video processor

---
+++ video.py
@@ -0,0 +1,16 @@
+# Load video/image/webcam for processing
+# Written by: JP Aldama
+# Date: 3/13/2020 10:06 pm
+# Github: https://github.com/sysad-aldama/emotion-detection
+# Email me: quaxiscorp@gmail.com
+# Copyright Quaxis Corporation (c) 2020
+# Special thanks to: Neha Yadav
+
+import os, cv2
+import numpy as np
+from keras.models import model_from_json
+from keras.preprocessing import image
+
+model = model_from_json()
+
+
